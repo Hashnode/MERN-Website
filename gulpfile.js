40ef01719e4892d96ffe91d2530a0757994fd51f
Syed Fazle Rahman <fazlerahman786@gmail.com>
2016-02-17 00:07:22 +0530

    Favicon changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    cssnano = require('gulp-cssnano');	
 
 gulp.task('less', function() {
     gulp.src('less/app.less')
         .pipe(less())
+	.pipe(cssnano())
         .pipe(gulp.dest('css/'))
 });
 
