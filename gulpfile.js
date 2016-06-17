58452c87215ea21e4b930232f4b4110e7a0bf6ae
Syed Fazle Rahman <fazle@hashnode.com>
2016-06-17 16:51:38 +0530

    project cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    cssnano = require('gulp-cssnano');	
+  less = require('gulp-less'),
+  path = require('path'),
+  watch = require('gulp-watch'),
+  uglifycss = require('gulp-uglifycss');
 
-gulp.task('less', function() {
-    gulp.src('less/app.less')
-        .pipe(less())
-	    .pipe(cssnano())
-        .pipe(gulp.dest('css/'))
+gulp.task('default', function() {
+  
+});
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(uglifycss())
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('less/app.less', ['less']);
+    gulp.watch('./less/*.less', ['less']);
 });
